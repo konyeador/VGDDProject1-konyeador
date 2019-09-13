365b4eb34ba7630c7d5b71b471391bbf7a5d5c42
Kachi Onyeador <kachionyeador@gmail.com>
2019-09-12 22:17:22 -0700

    submission

---
+++ Assets/Scripts/Player/PlayerFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private Transform m_PlayerTransform;
+
+    [SerializeField]
+    private Vector3 m_offset;
+ 
+    [SerializeField]
+    private float m_RotationSpeed = 10;
+    #endregion
+
+    #region Main Updates
+    private void LateUpdate()
+    {
+        Vector3 newPos = m_PlayerTransform.position + m_offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, 1);
+        float rotationAmount = m_RotationSpeed * Input.GetAxis("Mouse X");
+        transform.RotateAround(m_PlayerTransform.position, Vector3.up, rotationAmount);
+
+        m_offset = transform.position - m_PlayerTransform.position;
+    }
+    #endregion
+}
