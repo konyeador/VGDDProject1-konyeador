afe58a34bf3fffe1de6e560b7ee1f879461bbced
Kachi Onyeador <kachionyeador@gmail.com>
2019-09-22 22:20:30 -0700

    part 2

---
+++ Assets/Scripts/Misc/Soundscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Soundscript : MonoBehaviour
+{
+    private MusicScript music;
+    public Button musicToggleButton;
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+
+    private void Start()
+    {
+        music = GameObject.FindObjectOfType<MusicScript>();
+        UpdateIcon();
+    }
+
+    public void PauseMusic()
+    {
+        music.ToggleSound();
+        UpdateIcon();
+    }
+
+    void UpdateIcon()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            musicToggleButton.GetComponent<Image>().sprite = musicOnSprite;
+        } else
+        {
+            AudioListener.volume = 0;
+            musicToggleButton.GetComponent<Image>().sprite = musicOffSprite;
+        }
+    }
+}
